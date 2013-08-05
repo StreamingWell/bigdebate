e2914e2c0e43694468277f7c868ca273f94792be
Branden Mittra <branden.mittra@streamingwell.com>
2013-08-05 14:29:58 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bigdebate::Application.config.secret_token = 'a485011fb369f7ec037e17e94f09530c54944f235527b9213b2939e136a3bd77b6cd3030cb77204dfbbd42861c65a35c57a3e727d21f5fca443d257841e52ce9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bigdebate::Application.config.session_store :cookie_store, key: '_bigdebate_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bigdebate::Application.config.session_store :active_record_store
